051b0409ccf59ee9f12027446488c7def4a084cc
Calvin Caulfield <calvin.caulfield@gmail.com>
2021-08-13 04:12:33 +0000

    Give up runing oracle docker

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-oracle
 
 go 1.16
+
+require github.com/mattn/go-oci8 v0.1.1 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	userName = "admin"
+	passWd   = "calvin1729"
+)
+
+func main() {
+	fmt.Println("main")
+
+}
